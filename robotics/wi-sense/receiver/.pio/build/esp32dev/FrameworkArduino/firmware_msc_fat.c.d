5dfe6a06a30df8e2e4a1b96b35752f165435c784
ryan <ryanbaylosisbarro@gmail.com>
2026-09-09 22:15:03 +0800

    Restore robotics project

---
+++ robotics/wi-sense/receiver/.pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.o: \
+ C:/Users/ryan/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
+ C:/Users/ryan/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
